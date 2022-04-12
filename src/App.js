4da955316a8687938a1456786bd402c3f4aee851
msh0108 <audtmdgur@naver.com>
2022-04-13 03:38:22 +0900

    Chapter4: 변수말고 state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+let [글제목, 글제목변경] = useState(['남자 코트 추천' ,'강남 우동 맛집']);
 
 let posts = '강남 고기 맛집';
 
   return (
     <div className="App">
       <div className='black-nav'> {/* jsx */}
-        <div style={ { color : 'blue', fontSize : '30px' } }>개발 Blog</div>
+        <div>개발 Blog</div>
       </div>
-      <img src={ logo } />
-      <h4> { posts } </h4>
+      <div className='list'>
+      <h3> { 글제목[0] } </h3>
+      <p>2월 17일 발행</p>
+      <hr/>
+      </div>
     </div>
   );
 }
