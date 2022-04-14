b0827288a02517b1594dd9ef4a42c8e0a4b0a6b4
msh0108 <audtmdgur@naver.com>
2022-04-15 03:14:02 +0900

    Chapter6: state 변경시 deep copy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,21 @@ import './App.css';
 
 function App() {
 
-let [글제목, 글제목변경] = useState(['남자 코트 추천' ,'강남 우동 맛집']);
+let [글제목, 글제목변경] = useState(['남자 코트 추천' ,'강남 우동 맛집', '벚꽃 구경 핫플']);
+
+let [따봉, 따봉변경] = useState(0);
+
+function 제목바꾸기(){
+  var newArray = [...글제목]; 
+  newArray[0] = '여자 코트 추천'
+  글제목변경( newArray );
+}
+
+function 제목정렬(){
+  var newArray = [...글제목]; 
+  newArray = newArray.sort();
+  글제목변경( newArray );
+}
 
 let posts = '강남 고기 맛집';
 
@@ -13,9 +27,20 @@ let posts = '강남 고기 맛집';
       <div className='black-nav'> {/* jsx */}
         <div>개발 Blog</div>
       </div>
+      <button onClick={ 제목정렬 }>버튼</button>
       <div className='list'>
-      <h3> { 글제목[0] } </h3>
+      <h3> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h3>
       <p>2월 17일 발행</p>
+      <hr/>
+      </div>
+      <div className='list'>
+      <h3> { 글제목[1] } </h3>
+      <p>2월 18일 발행</p>
+      <hr/>
+      </div>
+      <div className='list'>
+      <h3> { 글제목[2] } </h3>
+      <p>2월 19일 발행</p>
       <hr/>
       </div>
     </div>
