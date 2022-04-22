5b834de3e8ae5758c2df10d752b46fca325136d1
msh0108 <audtmdgur@naver.com>
2022-04-23 05:07:58 +0900

    Chapter10: props

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ let posts = '강남 고기 맛집';
         <div>개발 Blog</div>
       </div>
       {/* <button onClick={ 제목정렬 }>버튼</button> */}
-      <div className='list'>
+      {/* <div className='list'>
       <h3> { 글제목[0] } <span onClick={ () => { 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h3>
       <p>2월 17일 발행</p>
       <hr/>
@@ -50,10 +50,10 @@ let posts = '강남 고기 맛집';
       <h3> { 글제목[2] } </h3>
       <p>2월 19일 발행</p>
       <hr/>
-      </div>
-      <button onClick={ () => { modal변경(!modal) } }>버튼</button>
+      </div> */}
+      {/* <button onClick={ () => { modal변경(!modal) } }>버튼</button> */}
 
-      {
+      {/* {
         글제목.map(function(글, i){
           return (
           <div className='list'>
@@ -66,11 +66,23 @@ let posts = '강남 고기 맛집';
           </div>
           )
         })
+      } */}
+
+      {
+        글제목.map(function(글, i){
+          return (
+          <div className='list' key={i}>
+          <h3 onClick={ () => {modal변경(true)} }> { 글제목[i] } <span onClick={ () => {따봉변경(따봉+1)} }>👍</span> {따봉} </h3>
+          <p>2월 17일 발행</p>
+          <hr/>
+          </div>
+          )
+        })
       }
 
       {
         modal === true
-        ? <Modal />
+        ? <Modal 글제목={글제목}/>
         : null
       }
       
@@ -81,12 +93,13 @@ let posts = '강남 고기 맛집';
 
 }
 
-function Modal(){
+function Modal(props){
   return (
-    <div>
-        <h2>제목</h2>
+    <div className='modal'>
+        <h2>{props.글제목[0]}</h2>
         <p>날짜</p>
         <p>상세내용</p>
+        <button>글수정</button>
       </div>
   )
 }
